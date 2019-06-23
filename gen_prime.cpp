6cbcc144e6477878d9e4b4bfd79c7618c18b835d
Akil <krs.akil@gmail.com>
2019-06-23 11:14:17 +0000

    Initial commit

---
+++ gen_prime.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+int main()
+{
+	long i = 0;
+	bool isprime = true;
+	while (true)
+	{
+		i++;
+		for (long j = 2; j * j <= i; j++)
+		{
+			if (i % j == 0)
+			{
+				isprime = false;
+				break;
+			}
+			else
+			{
+				isprime = true;
+			}
+		}
+
+		if (isprime) {
+			//std::cout << "Prime: " << i << "\r\n";
+			printf("Prime: %d.\r\n", i);
+		}
+	}
+	return 0;
+
+}
